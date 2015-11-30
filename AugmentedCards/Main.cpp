ed5f5f85fc99b4199431353e66e06e47ba66b1be
Vasco Gomes <ovascogomes@gmail.com>
2015-11-30 16:28:45 +0000

    Added method to detect contours

--- AugmentedCards/Main.cpp
+++ AugmentedCards/Main.cpp
@@ -7,35 +7,74 @@
 using namespace cv;
 using namespace std;
 
+bool compareContourArea(vector<Point> i, vector<Point> j);
+
 int main(int argc, char** argv)
 {
-	VideoCapture cap(0);
+	RNG rng(12345);
+	Mat original, processing, render;
+	vector<Vec4i> hierarchy;
+	vector<vector<Point>> contours;
+	int nCards = 2;
 
-	if (!cap.isOpened())
+	namedWindow("original", 1);
+	namedWindow("processing", 1);
+	namedWindow("render", 1);
+
+	// read from image
+	original = imread("../Assets/cards.jpg", IMREAD_COLOR);
+
+		if (original.empty())
 	{
+		cout << "Could not open or find the image" << endl;
 		return -1;
 	}
 
-	Mat original;
-	Mat edges;
+	// grayscale, blur, threshold
+	cvtColor(original, processing, CV_BGR2GRAY);
+	GaussianBlur(processing, processing, Size(1, 1), 1000);
+	threshold(processing, processing, 120, 255, THRESH_BINARY);
 
-	namedWindow("original", 1);
-	namedWindow("edges", 1);
+	// edge detection and contours
+	Canny(processing, processing, 0, 60, 3);
+	findContours(processing, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
 
-	while (cap.isOpened())
+	// check for number of cards
+	if ((int) contours.size() < nCards)
 	{
-		cap >> original;
-		cvtColor(original, edges, CV_BGR2GRAY);
-		GaussianBlur(edges, edges, Size(7, 7), 1.5, 1.5);
-		Canny(edges, edges, 0, 30, 3);
-
-		imshow("edges", edges);
-		imshow("original", original);
-
-		if (waitKey(30) >= 0) {
-			break;
-		}
+		return 0;
 	}
 
+	// sorting by largest area
+	sort(contours.begin(), contours.end(), compareContourArea);
+
+	// draw detected cards
+	render = Mat::zeros(processing.size(), CV_8UC3);
+
+	for (int i = 0; i < nCards; i++)
+	{
+		Scalar color = Scalar(rng.uniform(0, 255), rng.uniform(0, 255), rng.uniform(0, 255));
+		drawContours(render, contours, i, color, 1, CV_AA, hierarchy, 0, Point());
+
+		for (unsigned int x = 0; x < contours[i].size(); x++)
+		{
+			// cout << "Point " << x << " - x: " << contours[i][x].x << " | y: " << contours[i][x].y << endl;
+		}
+
+		cout << endl << endl << endl;
+	}
+
+	imshow("original", original);
+	imshow("processing", processing);
+	imshow("render", render);
+
+	waitKey(0);
 	return 0;
+}
+
+bool compareContourArea(vector<Point> i, vector<Point> j)
+{
+	// second parameter controls the orientation
+	// "true" avoids duplicates when sorting
+	return contourArea(i, true) > contourArea(j, true);
 }
